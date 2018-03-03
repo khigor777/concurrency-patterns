c1718b771402804aa3a8fd8068627d6348503be7
Igor Khristenko <khigor777@gmail.com>
2018-03-03 13:04:50 +0300

    add chan_2 example

--- chanel/chan_2.go
+++ chanel/chan_2.go
@@ -1,4 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
+	ch := make(chan int)
+
+	go func(ch chan<- int) {
+
+		for i := 0; i < 5; i++ {
+			fmt.Println("before ", i)
+			ch <- i
+			fmt.Println("after", i)
+		}
+		close(ch) //we mast close chanel if we want rea it with range
+		fmt.Println("finsh write to chanel")
+
+	}(ch)
+
+	for i := range ch {
+		fmt.Println("read chanel item: ", i)
+	}
 }
